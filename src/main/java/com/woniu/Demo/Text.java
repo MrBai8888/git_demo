e59bf2e6f741039c9c897f3293e3df7d83c3c8cd
Mr.bai <526943623@qq.com>
2022-07-11 15:37:47 +0800

    git第二次提交

---
+++ src/main/java/com/woniu/Demo/Text.java
@@ -0,0 +1,15 @@
+package com.woniu.Demo;
+
+/**
+ * @author Mr.bai~
+ * @version 1.0
+ * @date 2022/7/11 15:03
+ */
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
+        System.out.println("44444");
+    }
+}
